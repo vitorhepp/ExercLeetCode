1f7542552bd9cbfb90c3f2265d636bbd5f1a06ca
vitorhepp <114616170+vitorhepp@users.noreply.github.com>
2023-07-07 15:01:23 -0300

    Create trabalho.c

---
+++ trabalho.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char * shortestCompletingWord(char * licensePlate, char ** words, int wordsSize);
+
+int main(){
+    char lincensePlate[7] = "1s3 456";
+    char words[][10] = {"step","steps","stripe","stepple"};
+
+}
+
+char * shortestCompletingWord(char * licensePlate, char ** words, int wordsSize){
+    int i, j;
+    char shortword[], word[];
+    for(i=0;i<words[];i++;){
+        wordsSize = strlen(words[]);
+            for(j=0;j<wordsSize;j++){
+
+            j++;
+            }
+        if(shortword[]<word[]){
+            shortword[]=word[];
+            }
+        i++;
+    }
+    return shortword;
+}
